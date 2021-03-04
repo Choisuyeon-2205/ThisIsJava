7945051bb0f3eaf190ba5e56c147db0ff0014e8d
Choisuyeon-2205 <58283314+Choisuyeon-2205@users.noreply.github.com>
2021-03-04 17:45:51 +0900

    Rename Hello.java to Day01/Hello.java

---
+++ Day01/Hello.java
@@ -0,0 +1,16 @@
+package com.kosta.day01;
+
+public class Hello {
+
+	//프로그램 시작점: main
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("첫번째 자바 프로그램0");
+		System.out.println("첫번째 자바 프로그램1");
+		System.out.println("첫번째 자바 프로그램2");
+		System.out.println("첫번째 자바 프로그램3");
+		
+	}
+
+}
