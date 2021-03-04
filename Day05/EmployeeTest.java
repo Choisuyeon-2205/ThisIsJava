ef03cb5743961191af9c76c6dd73a4e177aebc18
Choisuyeon-2205 <58283314+Choisuyeon-2205@users.noreply.github.com>
2021-03-04 17:51:09 +0900

    Rename EmployeeTest.java to Day05/EmployeeTest.java

--- Day05/EmployeeTest.java
+++ Day05/EmployeeTest.java
@@ -4,14 +4,14 @@ import com.kosta.day04.Employee;
 public class EmployeeTest {
 	 public static void main(String[] args) {
 		 Employee emp1= new Employee();
-		 Employee emp2= new Employee(2,"È«±æµ¿");
-		 Employee emp3= new Employee(3,"±è±æµ¿","°³¹ßºÎ");
-		 Employee emp4= new Employee(4,"¹Ú±æµ¿","±³À°ºÎ","010-8888-9999");
+		 Employee emp2= new Employee(2,"í™ê¸¸ë™");
+		 Employee emp3= new Employee(3,"ê¹€ê¸¸ë™","ê°œë°œë¶€");
+		 Employee emp4= new Employee(4,"ë°•ê¸¸ë™","êµìœ¡ë¶€","010-8888-9999");
 		 
 		 emp1.changePhone("010-2222-3333");
 		 emp1.print();
 		 
-		 //º¯¼ö°©½½ Á÷Á¢ ¼öÁ¤ÇÏ´Â °ÍÀº ÇÇÇØ¾ßÇÔ => ¸Ş¼­µå »ç¿ëÇÏ±â!!
+		 //ë³€ìˆ˜ê°‘ìŠ¬ ì§ì ‘ ìˆ˜ì •í•˜ëŠ” ê²ƒì€ í”¼í•´ì•¼í•¨ => ë©”ì„œë“œ ì‚¬ìš©í•˜ê¸°!!
 		 emp2.changePhone("010-5555-6666");
 		 emp2.print();
 		 
@@ -19,7 +19,7 @@ public class EmployeeTest {
 		 System.out.println(emp4.getEmpInfo());
 		 
 		 String aa= emp4.changePhone2("000-0000-0000");
-		 System.out.println("¹ŞÀº °ªÀº "+aa);
+		 System.out.println("ë°›ì€ ê°’ì€ "+aa);
 		 
 	 }
 }
