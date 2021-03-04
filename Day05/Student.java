02f7bca8604e4d53bfa67e8438c1299be1bf6a9c
Choisuyeon-2205 <58283314+Choisuyeon-2205@users.noreply.github.com>
2021-03-04 17:51:29 +0900

    Add files via upload

---
+++ Day05/Student.java
@@ -0,0 +1,10 @@
+package com.kosta.day05;
+
+public class Student extends People{
+	public int studentNo;
+	
+	public Student(String name, String ssn, int studentNo) {
+		super(name,ssn);
+		this.studentNo= studentNo;
+	}
+}
