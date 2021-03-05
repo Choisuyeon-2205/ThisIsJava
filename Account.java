9a4214539b9c1fa4776870e84be5731253ba75c4
Choisuyeon-2205 <58283314+Choisuyeon-2205@users.noreply.github.com>
2021-03-05 17:36:39 +0900

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,26 @@
+package com.kosta.day06;
+
+public class Account {
+	private int balance;
+	static final int MIN_BALANCE= 0;
+	static final int MAX_BALANCE= 1000000;
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(int balance) {
+		
+		if(balance>=MIN_BALANCE && balance<=MAX_BALANCE)
+			this.balance = balance;
+		
+//		if(balance<MIN_BALANCE || balance>MAX_BALANCE) {
+//			return; //»ý·«
+//		}else {
+//			this.balance = balance;
+//		}
+			
+	}
+	
+	
+}
