48c691fd6c7622a036d6e39152fed07faa3e2c74
Choisuyeon-2205 <58283314+Choisuyeon-2205@users.noreply.github.com>
2021-03-08 17:08:42 +0900

    Add files via upload

---
+++ Day14/Ex/ArrayAsListExample.java
@@ -0,0 +1,19 @@
+package com.kosta.day14.Ex;
+import java.util.*;
+
+public class ArrayAsListExample {
+
+	public static void main(String[] args) {
+		List<String> list1= Arrays.asList("È«±æµ¿","½Å¿ë±Ç","±èÀÚ¹Ù");
+		for(String name: list1) {
+			System.out.println(name);
+		}
+		
+		List<Integer> list2= Arrays.asList(1,2,3);
+		for(int value: list2) {
+			System.out.println(value);
+		}
+		list2.add(4);
+	}
+
+}
